c134f0ec25f773ee99dbd4bee3ac2b55dec42213
Mukesh Sahani <52211262+MkshSahani@users.noreply.github.com>
2023-10-08 15:05:43 +0530

    Create user_validator.py

---
+++ users/user_validator.py
@@ -0,0 +1,19 @@
+# filename : user_validators.py
+from pydantic import BaseModel 
+from typing import Optional
+class UserValidator(BaseModel): 
+
+    username    : str
+    firstname   : str 
+    middle_name : Optional[str]
+    lastname    : Optional[str]
+    password    : str 
+    email       : str 
+    phone       : str 
+    user_type   : int 
+
+class UserLoginValidator(BaseModel): 
+
+    username    : str
+    password    : str 
+
